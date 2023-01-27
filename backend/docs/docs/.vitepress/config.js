14ab434f4e10afdee9fc56a1e75fa2a02412cad0
Jotter Kain <jotterkain@gmail.com>
2023-01-28 00:15:29 +0100

    docker-compose multi-container

---
+++ backend/docs/docs/.vitepress/config.js
@@ -0,0 +1,5 @@
+export default {
+    title: 'Banking System',
+    description: 'A banking system management backend written in spring boot by Jotter Kain - A school project',
+    cleanUrls: 'without-subfolders'
+}
